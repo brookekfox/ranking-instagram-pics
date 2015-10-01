0625b1d8a07e8c8c2da32c6adbd67d1f9d28904d
brookekfox <brookekfox@gmail.com>
2015-09-30 23:02:17 -0700

    pull request out to factory, fix css

--- js/instaControllers.js
+++ js/instaControllers.js
@@ -1,48 +1,56 @@
-//http://stackoverflow.com/questions/28687931/cannot-use-ng-repeat-with-a-json-from-ajax-call
-
 angular.module("instagram", [])
-.controller("InstagramController", ["$scope", function ($scope) {
+.controller("InstagramController", ["$scope", "RequestService", function ($scope, RequestService) {
   $scope.user = {
     handle: "",
     id: "",
-    fullName: "",
     photos: []
   };
 
   $scope.sendInstaRequest = function(handle) {
-    $scope.user.handle = handle;
-
-    $.ajax({
-      type: "GET",
-      dataType: "jsonp",
-      cache: false,
-      url: "https://api.instagram.com/v1/users/search?access_token=184890229.1fb234f.8536822dba9e4fa198a10d45e434da91&q=" + $scope.user.handle,
-      success: function (response) {
-        $scope.user.id = response.data[0].id.toString();
-        $scope.user.fullName = response.data[0].full_name;
-        console.log($scope.user);
-
-        $.ajax({
-          type: "GET",
-          dataType: "jsonp",
-          cache: false,
-          url: "https://api.instagram.com/v1/users/" + $scope.user.id +
-          "/media/recent?access_token=184890229.1fb234f.8536822dba9e4fa198a10d45e434da91",
-          success: function (response) {
-            for (var i = 0; i < response.data.length; i++) {
-              $scope.user.photos[i] = {
-                caption: response.data[i].caption,
-                likes: response.data[i].likes.count,
-                image: response.data[i].images.low_resolution.url
-              }
-            }
-            $scope.user.photos = $scope.user.photos.sort(function (a, b) {
-              return b.likes - a.likes;
-            });
-          }
-        });
+    RequestService.fetch(handle).then(function(response) {
+      $scope.user.handle = handle;
+      $scope.user.photos = [];
+      for (var i = 0; i < response.data.length; i++) {
+        $scope.user.photos[i] = {
+          caption: response.data[i].caption,
+          likes: response.data[i].likes.count,
+          image: response.data[i].images.low_resolution.url
+        }
       }
+      $scope.user.photos = $scope.user.photos.sort(function (a, b) {
+        return b.likes - a.likes;
+      });
     });
   }
+}])
+.factory("RequestService", ["$http", "$q", function($http, $q){
+  return {
+    fetch: function(handle) {
+      var defer = $q.defer();
+      var user = {
+        handle: handle,
+        id: ""
+      };
+      $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/search?access_token=184890229.1fb234f.8536822dba9e4fa198a10d45e434da91&q=" + user.handle,
+        success: function (response) {
+          user.id = response.data[0].id.toString();
 
+          $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            url: "https://api.instagram.com/v1/users/" + user.id + "/media/recent?access_token=184890229.1fb234f.8536822dba9e4fa198a10d45e434da91",
+            success: function (response) {
+              defer.resolve(response);
+            }
+          });
+        }
+      });
+      return defer.promise;
+    }
+  }
 }]);
